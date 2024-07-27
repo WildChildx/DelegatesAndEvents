4aa417a11497afc3282bc65691b5a30b038bd365
Shree Chatane <58407535+WildChildx@users.noreply.github.com>
2024-07-27 15:47:36 +0530

    Dynamic way of assigning data to delegate

--- Program.cs
+++ Program.cs
@@ -6,21 +6,26 @@
         static void Main(string[] args)
         {
             WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
 
-
-            del1(5, WorkType.GenerateReports);
+            DoWork(del1);
 
             Console.Read();
         }
 
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.Golf);
+        }
+
         static void WorkPerformed1(int hours, WorkType workType)
         {
-            Console.WriteLine("Work performed 1 called");
+            Console.WriteLine("Work performed 1 called " + hours);
         }
 
         static void WorkPerformed2(int hours, WorkType workType)
         {
-            Console.WriteLine("Work performed 2 called");
+            Console.WriteLine("Work performed 2 called " + hours);
         }
     }
 
